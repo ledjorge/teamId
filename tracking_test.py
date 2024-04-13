2ba8137c1f91c772895c5383869dff3285df7924
ledjorge <ledjorge@gmail.com>
2024-04-13 18:50:04 +0200

    Update tracking_test.py
    - Added other methods to extract features

--- tracking_test.py
+++ tracking_test.py
@@ -3,29 +3,64 @@ import supervision as sv
 from ultralytics import YOLO
 import utils
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 
-model = YOLO(model="yolov8n.pt")
+model_det = YOLO(model="yolov8n.pt")
 tracker = sv.ByteTrack()
 box_annotator = sv.BoundingBoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 trace_annotator = sv.TraceAnnotator()
 
+#
+train_images = []
+
+#Using conrtrastive learning model for features
+method = 'hist'  #'hist', 'bag', 'ae'
+model_cl_path = '/Users/jonino/src/teamId/trained_models/embedding.pth'
+model_cl = utils.load_model_embed(model_cl_path)
+
+#Classifier
 kmeans = KMeans(n_clusters=2)
-labels_k_means_names = ['teamA', 'teamB']
+labels_k_means_names = ['teamA', 'teamB', 'referee', 'fans']
+
+
+def get_bag_features(train_images, test_images, K=35, h=64, pixel_number=400000):
+    # learn bag of colors on the current game
+    flat = []
+    for i, c in enumerate(train_images):
+        c = c[:h, :, :]
+        r = c[:, :, 0].flatten()
+        b = c[:, :, 1].flatten()
+        g = c[:, :, 2].flatten()
+        temp = np.dstack((r, b, g))
+        non_black_pixels_mask = np.any(temp != [0, 0, 0], axis=-1)
+        flat.extend(temp[non_black_pixels_mask].tolist())
+        if len(flat) > pixel_number:
+            break
+    if len(flat) > pixel_number:
+        flat = flat[:pixel_number]
+    gmm = GaussianMixture(n_components=K).fit(flat)
+
+    X_train = utils.get_hist_from_gmm(gmm, train_images, K=K)
+    X_test = utils.get_hist_from_gmm(gmm, test_images, K=K) if test_images is not None else None
+    return X_train, X_test
 
 def callback(frame: np.ndarray, n_frame: int) -> np.ndarray:
-    results = model(frame, device='mps')[0]
+    results = model_det(frame, device='mps')[0]
     detections = sv.Detections.from_ultralytics(results)
     detections = tracker.update_with_detections(detections)
     if len(detections) == 0: return frame
 
     if n_frame == 0:
-        train_images = [
-            frame[int(xyxy[1]):int(xyxy[3]), int(xyxy[0]):int(xyxy[2])]
-            for xyxy
-            in detections.xyxy
-        ]
-        train_features = utils.get_hist_features(train_images)
+        train_images.extend([
+            frame[max(int(xyxy[1]), 0):max(int(xyxy[3]), 0), max(int(xyxy[0]), 0):max(int(xyxy[2]), 0)]
+            for xyxy in detections.xyxy
+        ])
+
+        if (method == 'hist'): train_features = utils.get_hist_features(train_images)
+        elif (method == 'bag'): train_features, _ = get_bag_features(train_images, None)
+        else: train_features = utils.get_features(train_images, model_cl)
+
         kmeans.fit(train_features)
 
     test_images = [
@@ -33,11 +68,15 @@ def callback(frame: np.ndarray, n_frame: int) -> np.ndarray:
         for xyxy
         in detections.xyxy
     ]
-    test_features = utils.get_hist_features(test_images)
+
+    if (method == 'hist'): test_features = utils.get_hist_features(test_images)
+    elif (method == 'bag'): train_features, test_features = get_bag_features(train_images, test_images)
+    else: test_features = utils.get_features(test_images, model_cl)
 
     labels_k_means = kmeans.predict(test_features)
 
     # Output
+    detections.class_id = labels_k_means
     labels = [
         f"#{tracker_id} {labels_k_means_names[label_k]}"
         for tracker_id, label_k
@@ -52,6 +91,6 @@ def callback(frame: np.ndarray, n_frame: int) -> np.ndarray:
 
 sv.process_video(
     source_path="/Users/jonino/Documents/personal/cv/ml6/senior-ml-engineer-challenge/sample.mp4",
-    target_path="/Users/jonino/tests/ml6/result_ml6_challenge.mp4",
+    target_path="/Users/jonino/tests/ml6/result_ml6_challenge_hist_2.mp4",
     callback=callback
 )
